de0edf396b7aff77d54203dcd01130debfeb325e
sootn <encrypt.wave@gmail.com>
2014-06-03 18:09:30 +0200

    Initial Commit

---
+++ Swifty-elo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swifty-elo
+//
+//  Created by Andreas on 03/06/14.
+//  Copyright (c) 2014 Andreas. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
